1d1adf0a3993484f3d2651aed57810548339fb00
1063263175 <11@qq.com>
2019-01-19 16:13:23 +0800

    1.19抽奖
    Signed-off-by: 1063263175 <11@qq.com>

--- app/admin/controller/Shop.php
+++ app/admin/controller/Shop.php
@@ -15,12 +15,129 @@ class Shop extends Permissions
 {
     public function choujiang()
     {
-        
+        $list=Db::name('jiang')->paginate(20);
+        $this->assign('list',$list);
+        return $this->fetch();
     }
 
     public function cjpublish()
     {
-        
+        //获取菜单id
+        $id = $this->request->has('jiang_id') ? $this->request->param('jiang_id', 0, 'intval') : 0;
+        //是正常添加操作
+        if($id > 0) {
+            //是修改操作
+            if($this->request->isPost()) {
+                //是提交操作
+                $post = $this->request->post();
+                //验证  唯一规则： 表名，字段名，排除主键值，主键名
+                $validate = new \think\Validate([
+                    ['title', 'require', '名称不能为空'],
+                    ['jiang_number', 'require', '抽奖个数不能为空'],
+                    ['kai_time', 'require', '截止时间不能为空'],
+                ]);
+                //验证部分数据合法性
+                if (!$validate->check($post)) {
+                    $this->error('提交失败：' . $validate->getError());
+                }
+                $post['kai_time']=strtotime($post['kai_time']);
+                //验证菜单是否存在
+/*                $cate = Db->where('id',$id)->find();
+                if(empty($cate)) {
+                    return $this->error('id不正确');
+                }*/
+                if(false == Db::name('jiang')->where(['jiang_id'=>$id])->update($post)) {
+                    return $this->error('修改失败');
+                } else {
+                    return $this->success('修改成功','admin/shop/choujiang');
+                }
+            } else {
+                //非提交操作
+                $cate=Db::name('jiang')->where('jiang_id',$id)->find();
+                $music=Db::name('music')->field('music_id,name')->select();
+                $this->assign('cate',$cate);
+                if(!empty($cate)) {
+                    $this->assign('music',$music);
+                    return $this->fetch();
+                } else {
+                    return $this->error('id不正确');
+                }
+            }
+        } else {
+            //是新增操作
+            if($this->request->isPost()) {
+                //是提交操作
+                $post = $this->request->post();
+                //验证  唯一规则： 表名，字段名，排除主键值，主键名
+                $validate = new \think\Validate([
+                    ['title', 'require', '名称不能为空'],
+                    ['jiang_number', 'require', '抽奖个数不能为空'],
+                    ['kai_time', 'require', '截止时间不能为空'],
+                ]);
+                //验证部分数据合法性
+                if (!$validate->check($post)) {
+                    $this->error('提交失败：' . $validate->getError());
+                }
+                $post['add_time']=time();
+                $post['kai_time']=strtotime($post['kai_time']);
+                if(false == Db::name('jiang')->insert($post)) {
+                    return $this->error('添加失败');
+                } else {
+                    return $this->success('添加成功','admin/shop/choujiang');
+                }
+            } else {
+                //非提交操作
+                $music=Db::name('music')->field('music_id,name')->select();
+                $this->assign('music',$music);
+                return $this->fetch();
+            }
+        }
+    }
+
+
+    /*
+     * 开奖
+     * */
+    public function kai()
+    {
+        $jiang_id=input('jiang_id');
+        $jiang=Db::name('jiang')->where('jiang_id',$jiang_id)->find();
+        if ($jiang['status']==0){
+            $code_list=Db::name('jiang_code')->where('jiang_id',$jiang_id)->select();
+            $code_count=count($code_list);
+
+            //dump($code_list);
+            //dump($code_count);die;
+            if ($code_count>$jiang['jiang_number']){
+                $res_code=array_rand($code_list,$jiang['jiang_number']);
+            }else{
+                $res_code=$code_count;
+            }
+            if (is_array($res_code)){
+                foreach ($res_code as $k=>$v){
+                    //echo '12';
+                    $data[$k]=[
+                        'jiang_code'=>$code_list[$v]['jiang_code'],
+                        'jiang_id'=>$jiang_id
+                    ];
+                }
+                //dump($data);
+                Db::name('jiang_res')->insertAll($data);
+
+            }elseif (count($res_code)==1&$res_code>0){
+                Db::name('jiang_res')
+                    ->insert([
+                        'jiang_code'=>$code_list[$res_code]['jiang_code'],
+                        'jiang_id'=>$jiang_id
+                    ]);
+            }
+            //die;
+            Db::name('jiang')->where('jiang_id',$jiang_id)->update(['status'=>1]);
+            //die;
+            return $this->success('开奖成功','admin/shop/choujiang');
+        }else{
+            return $this->error('参数错误');
+        }
     }
 
     public function order()
--- app/index/controller/Shop.php
+++ app/index/controller/Shop.php
@@ -109,11 +109,11 @@ class Shop extends Base
             'user_id'=>$user_id,
             'jiang_id'=>$jiang_id
         ];
-        $res=Db::name('jianng_user')
+        $res=Db::name('jiang_user')
             ->where($info)
             ->find();
         if (empty($res)){
-            Db::name('jianng_user')
+            Db::name('jiang_user')
                 ->insert($info);
             $res['chou_number']=1;
         }
@@ -139,18 +139,157 @@ class Shop extends Base
             'user_id'=>$user_id,
             'jiang_id'=>$jiang_id,
         ];
-        $res=Db::name('jianng_user')->where('$info')->find();
+        $res=Db::name('jiang_user')->where('$info')->find();
         if (empty($res)){
             $info['chou_number']=$number+1;
-            Db::name('jianng_user')->insert($info);
+            Db::name('jiang_user')->insert($info);
             $num=$info['chou_number'];
         }else{
             $num=$number+$res['chou_number'];
-            $res=Db::name('jianng_user')->where($info)->update(['chou_number'=>$num]);
+            $res=Db::name('jiang_user')->where($info)->update(['chou_number'=>$num]);
         }
         return json(['title'=>'剩余抽奖次数','num'=>$num]);
     }
 
+
+    /**
+     * 获取抽奖码
+     * @param $user_id
+     * @param $jiang_id
+     * @return \think\response\Json
+     * @throws \think\Exception
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     * @throws \think\exception\PDOException
+     */
+    public function GetJiangCode($user_id, $jiang_id)
+    {
+        //判断抽奖次数
+        $where=[
+            'jiang_id'=>$jiang_id,
+            'user_id'=>$user_id,
+        ];
+        $ctime=Db::name('jiang')->where('jiang_id',$jiang_id)->value('kai_time');
+        if (time()>$ctime){
+            //抽奖时间超时
+            return $this->aerror('抽奖时间已过');
+        }
+
+        $num=Db::name('jiang_user')->where($where)->value('chou_number');
+        if ($num>0){
+            //抽奖次数减一
+            Db::name('jiang_user')->where($where)->update(['chou_number'=>$num-1]);
+        }else{
+            return $this->aerror('抽奖次数没了');
+        }
+
+        //读取已存在抽奖码确保唯一
+        $ex_code=Db::name('jiang_code')
+            ->where('jiang_id',$jiang_id)
+            ->field('jiang_code')
+            ->select();
+        //生成抽奖码
+        $code=$this->code($ex_code);
+        Db::name('jiang_code')->insert([
+           'user_id'=>$user_id,
+           'jiang_id'=>$jiang_id,
+           'jiang_code'=>$code,
+            'add_time'=>time(),
+        ]);
+        return json(['code'=>$code]);
+
+
+    }
+
+    /**
+     * 生成并验证抽奖码
+     * @param $ex_code
+     * @return string
+     */
+    function code($ex_code){
+        $code=$this->RandCode();
+        if (in_array($code,$ex_code)){
+            $this->code($ex_code);
+        }else{
+            return $code;
+        }
+    }
+
+
+    /**
+     * 生成随机字符串
+     * @return string
+     */
+    public function RandCode()
+    {
+        $code="ABCDEFGHIGKLMNOPQRSTUVWXYZ";
+        $rand=$code[rand(0,25)].strtoupper(dechex(date('m'))) .date('d').substr(time(),-5)  .substr(microtime(),2,5).sprintf('%02d',rand(0,99));
+        for(
+            $a = md5( $rand, true ),
+            $s = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+            $d = '',
+            $f = 0;
+            $f < 8;
+            $g = ord( $a[ $f ] ), // ord（）函数获取首字母的 的 ASCII值
+            $d .= $s[ ( $g ^ ord( $a[ $f + 8 ] ) ) - $g & 0x1F ],//按位亦或，按位与。
+            $f++
+        );
+        return $d;
+    }
+
+
+    /**
+     * 获取抽奖列表
+     * @param int $page
+     * @param int $pagelimit
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function GetJiangList($page=1,$pagelimit=20)
+    {
+        $list=Db::name('jiang')
+            ->page($page,$pagelimit)
+            ->order('jiang_id','desc')
+            ->select();
+        return json($list);
+    }
+
+    /**
+     * 获取抽奖信息详情
+     * @param $jiang_id
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function GetJiangInfo($jiang_id)
+    {
+        $info=Db::name('jiang')->where('jiang_id',$jiang_id)->find();
+        return json($info);
+    }
+
+
+    /**
+     * 获取抽奖结果
+     * @param $jiang_id
+     * @return \think\response\Json
+     * @throws \think\db\exception\DataNotFoundException
+     * @throws \think\db\exception\ModelNotFoundException
+     * @throws \think\exception\DbException
+     */
+    public function GetJiangRes($jiang_id)
+    {
+        $list=Db::name('jiang_res')
+            ->alias('jr')
+            ->join('tplay_jiang_code as jc','jc.jiang_code=jr.jiang_code','left')
+            ->join('tplay_user as tu','tu.user_id=jc.user_id','left')
+            ->where('jr.jiang_id',$jiang_id)
+            ->select();
+        return json($list);
+    }
 
 
 
